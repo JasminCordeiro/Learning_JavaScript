f5473f82130d7eaf8259636f626e72d9510442bd
Jasmin Cordeiro <79463331+Jassminand@users.noreply.github.com>
2023-12-02 20:42:13 -0300

    Update index2.js

--- Lista 1/index2.js
+++ Lista 1/index2.js
@@ -1,4 +1,4 @@
-
+//A partir de um número aleatório inteiro de 0 a 100, imprima o seu sucessor e antecessor.
 
 let valorAleatorio = Math.floor(Math.random() *100)
 console.log("O valor aleatório é: ", valorAleatorio)
